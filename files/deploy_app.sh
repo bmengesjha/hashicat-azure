d62d029c8608963858ab8c958d60951b9b0b032f
bmengesjha <59661090+bmengesjha@users.noreply.github.com>
2020-01-08 21:12:17 +0000

    Brian's add
    Brian's add

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,7 +10,7 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><h2>Meow World! Brian Was Here</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   MADE A CHANGE
   <!-- END -->
